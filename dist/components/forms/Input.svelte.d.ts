f4c2666375397e16dbbe57b68bf0f4a2bf84c071
Brain Who Codes <brainwhocodes@outlook.com>
2026-08-03 11:41:07 -0400

    fix: connect inputs to field context

--- dist/components/forms/Input.svelte.d.ts
+++ dist/components/forms/Input.svelte.d.ts
@@ -1,4 +1,25 @@
+import { SvelteComponentTyped } from "svelte";
 import type { HTMLInputAttributes } from 'svelte/elements';
-import { SvelteComponentTyped } from 'svelte';
-export type InputProps = HTMLInputAttributes & { class?: string; invalid?: boolean };
-export default class Input extends SvelteComponentTyped<InputProps, { [evt: string]: CustomEvent<any> }, {}> {}
+declare const __propDef: {
+    props: HTMLInputAttributes & {
+        class?: string;
+        invalid?: boolean;
+    };
+    events: {
+        input: InputEvent;
+        change: Event;
+        focus: FocusEvent;
+        blur: FocusEvent;
+        keydown: KeyboardEvent;
+        keyup: KeyboardEvent;
+    } & {
+        [evt: string]: CustomEvent<any>;
+    };
+    slots: {};
+};
+export type InputProps = typeof __propDef.props;
+export type InputEvents = typeof __propDef.events;
+export type InputSlots = typeof __propDef.slots;
+export default class Input extends SvelteComponentTyped<InputProps, InputEvents, InputSlots> {
+}
+export {};
